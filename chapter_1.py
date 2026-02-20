53c686e28a7d09edf05d309556fbbb265782eea6
ZIYAD AFTAB <ziyadyt94@gmail.com>
2026-02-20 11:45:20 -0800

    Add files via upload
    variable, Datatype, arithmetic, input , output

---
+++ chapter_1.py
@@ -0,0 +1,29 @@
+# question No 1
+
+a = int(input("first Num:"))
+b = int(input("Second Num:"))
+
+print(a+b)   
+
+# question No 2
+
+side = float(input("input the size of Square: "))
+print("area: ", side**2 )
+
+
+# question No 3
+
+x = float(input("Enter a first Number "))
+y = float(input("Enter a second Number "))
+
+sum = (x + y) / 2
+print("average ",sum)
+
+
+# question No 4
+
+x = int(input("Enter a first Number "))
+y = int(input("Enter a second Number "))
+
+print(x >= y)
+   
